a539e9ef8fe0ad4c0a49257a653db70f8261e0de
= <=>
2023-01-01 15:54:52 +0200

    Fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { QueryClient, QueryClientProvider } from 'react-query';
 import Employees from './components/employees/Employees';
 
 function App() {
-  const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Employees />
-    </QueryClientProvider>
-
+    <Employees />
   )
 }
 
