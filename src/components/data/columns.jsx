84edcfd881797766286d2d275aec45e2a1949715
= <=>
2023-01-08 13:36:05 +0200

    Fix responsive

--- src/components/data/columns.jsx
+++ src/components/data/columns.jsx
@@ -1,13 +1,21 @@
 export const columns = [
-    {
-        field: 'avatar', headerName: 'Avatar', width: 150, renderCell: (params) => {
-            return <img className='avatar' src={params.row.avatar} alt={`image - ${params.row.name}`} />
-
-        },
-    },
-    { field: 'id', headerName: 'ID', width: 150 },
-    { field: 'name', headerName: 'Full Name', width: 150 },
-    { field: 'date_birth', headerName: 'Date Birth', width: 140 },
-    { field: 'salary', headerName: 'Salary', width: 130 },
-    { field: 'at_work', headerName: 'At Work', width: 130 },
+	{
+		field: "avatar",
+		headerName: "Avatar",
+		width: 100,
+		renderCell: (params) => {
+			return (
+				<img
+					className="avatar"
+					src={params.row.avatar}
+					alt={`image - ${params.row.name}`}
+				/>
+			);
+		},
+	},
+	{ field: "id", headerName: "ID", width: 100 },
+	{ field: "name", headerName: "Full Name", width: 150 },
+	{ field: "date_birth", headerName: "Date Birth", width: 140 },
+	{ field: "salary", headerName: "Salary", width: 130 },
+	{ field: "at_work", headerName: "At Work", width: 130 },
 ];
