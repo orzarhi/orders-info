1d734ba1b30ce84aba623d4f9f5f17da8f7aac6e
= <=>
2023-01-11 13:27:50 +0200

    Clean code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { CssBaseline } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
-import DetailInfo from "./components/pages/detailInfo/DetailInfo";
+import Details from "./components/pages/details/Details";
 import Main from "./components/pages/main/Main";
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
 			<CssBaseline />
 			<Routes>
 				<Route path="/" element={<Main />} exact />
-				<Route path="/charts" element={<DetailInfo />} exact />
+				<Route path="/charts" element={<Details />} exact />
 			</Routes>
 		</>
 	);
